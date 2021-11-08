02d8e4ece09dd83a375c70d5de9299dba710da8e
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2021-11-08 14:46:47 -0800

    in a real blockchain situation, auction time would have to be checked before making a bid. The other conditions can be checked within updateBiddingPrice().

---
+++ Auction.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.9;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Auction{
+    function checkAucTime() public view{
+        
+    }
+    function updateBiddingPrice() public {
+        checkAucTime();
+    }
+    
+    
+}
