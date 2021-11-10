4e19b8f7981e974bc83858ec5a8de49e43361cc0
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2021-11-09 22:20:34 -0800

    wow my brain is in pain

--- Auction.sol
+++ Auction.sol
@@ -1,14 +1,52 @@
 // SPDX-License-Identifier: GPL-3.0
 
 pragma solidity >=0.8.9;
-
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+//I really dont want to test this lmao im too tired
 contract Auction{
-    function checkAucTime() public view{
+    
+    address owner;
+    uint256 currentbid;
+    uint startblock;
+    uint auctionEnd;
+    uint256 tokenid;
+    struct Bid {
+        address addr;
+        uint256 price;
+    }
+    Bid[] bidHistory;
+    constructor(){
+        owner = msg.sender;
+        startblock = block.number;
+        auctionEnd = startblock + 36000;
+    }
+    function aucEnding() view public returns (bool){
+        if(auctionEnd == block.number)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    function updateBiddingPrice(uint256 bid) public higherBid(bid){
+        require(!aucEnding(),"auction is already over");
+        require(bid<msg.sender.balance,"balance too low ur broke lol");
+        auctionEnd += 69;
+        currentbid = bid;
+        bidHistory.push(Bid(msg.sender,bid));
         
     }
-    function updateBiddingPrice() public {
-        checkAucTime();
+    function endAuction() public onlyOwner{
+        //something to do with transfer of token
+      //something to do with transfer of nft  IERC721.transferFrom(owner, bidHistory[bidHistory.length-1],tokenid);
     }
-    
-    
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not owner");
+        _;
+    }
+    modifier higherBid(uint256 bid){
+        require(bid>currentbid, "bid too low");
+        _;
+    }
 }
