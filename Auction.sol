4f27309a52040db0da12567125343d8cf87fbf2e
DidSomeoneSayBoba <aureliadufromage37@gmail.com>
2021-11-11 22:11:29 -0800

    i think it works
    i tried it but idk about transactions

--- Auction.sol
+++ Auction.sol
@@ -5,48 +5,77 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 //I really dont want to test this lmao im too tired
 contract Auction{
     
-    address owner;
-    uint256 currentbid;
+    address payable owner;
+    uint256 currentprice;
+    string description;
     uint startblock;
     uint auctionEnd;
     uint256 tokenid;
+    IERC721 public nft;
+    uint public nftId;
     struct Bid {
         address addr;
         uint256 price;
     }
     Bid[] bidHistory;
-    constructor(){
-        owner = msg.sender;
+    event bidUpdated(uint256 price, uint auctionEnd,Bid[] history);
+    constructor(address _nftaddress, uint _nftId, string memory _desc) descCheck(_desc){
+        nft = IERC721(_nftaddress);
+        nftId = _nftId;
+        description = _desc;
+        owner = payable (msg.sender);
         startblock = block.number;
         auctionEnd = startblock + 36000;
+        currentprice = 0;
+    }
+    modifier descCheck(string memory desc)
+    {
+        require(100>countWords(),"words too high");
+        _;
+    }
+    function countWords() public pure returns(uint256){
+        uint256 count = 0;
+       // for(uint256 a=0;a<bytes(words).length;a++){
+      //    if ( words[0] == " "){
+   //           count +=1;
+      //    }
+      //  }
+      return count;
     }
     function aucEnding() view public returns (bool){
-        if(auctionEnd == block.number)
-        {
-            return true;
-        }
-        else{
-            return false;
-        }
+        return (auctionEnd <= block.number);
+
     }
+    function timeLeft() view public returns(uint256){
+       return(auctionEnd- block.number);
+    }
+    function forceEndAuction() public{
+        auctionEnd = block.number;
+    }
+    event latest(address _address,uint256 price);
     function updateBiddingPrice(uint256 bid) public higherBid(bid){
         require(!aucEnding(),"auction is already over");
         require(bid<msg.sender.balance,"balance too low ur broke lol");
-        auctionEnd += 69;
-        currentbid = bid;
+        auctionEnd += 0;
+        currentprice = bid;
         bidHistory.push(Bid(msg.sender,bid));
-        
+        emit bidUpdated(currentprice, currentprice, bidHistory);
     }
-    function endAuction() public onlyOwner{
-        //something to do with transfer of token
-      //something to do with transfer of nft  IERC721.transferFrom(owner, bidHistory[bidHistory.length-1],tokenid);
+    function endAuction() payable public onlyOwner{
+        require(aucEnding(),"auction hasn't ended");
+        nft.safeTransferFrom(owner, bidHistory[bidHistory.length-1].addr,tokenid);
+        owner.transfer(msg.value); 
+    }
+    function latestBid() public returns(string memory){
+        emit latest(bidHistory[bidHistory.length-1].addr,bidHistory[bidHistory.length-1].price);
+        return "";
     }
     modifier onlyOwner() {
         require(msg.sender == owner, "Not owner");
         _;
     }
     modifier higherBid(uint256 bid){
-        require(bid>currentbid, "bid too low");
+        require(bid>currentprice, "bid too low");
         _;
     }
 }
